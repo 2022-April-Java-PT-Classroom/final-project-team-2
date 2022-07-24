ec00e1e89f29d8a1b682fa86dc304e8a5d2dfaa0
pennyschuenke <pennyschuenke@gmail.com>
2022-07-24 15:48:44 -0400

    setup

--- application/src/main/resources/static/js/react-client/src/pages/adhd/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/adhd/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Adhd ()=>{
+const Adhd =()=>{
     return("");
 }
 export default Adhd;
--- application/src/main/resources/static/js/react-client/src/pages/home/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/home/index.js
@@ -1,12 +1,15 @@
 import React from "react"
+import Adhd from "../../pages/home";
+import style from "./style.module.scss";
+
 
 const Home = () => {
     return(
         <div>
-            <div>
+            <div className={style.homegrid}>
                 <h1>Think Differently</h1>
                 <p>This is a paragraph</p>
-                <a href={/pagesadhd}>ADHD</a>
+                <a href={Adhd}>ADHD</a>
                 <button>Autism</button>
 
             </div>
