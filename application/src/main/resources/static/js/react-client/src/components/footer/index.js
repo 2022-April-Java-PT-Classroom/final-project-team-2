dda68b7d6d90a7444cefc0815868a20022bf7d4e
danefirestone <danefirestone@gmail.com>
2022-07-24 13:25:42 -0400

    Created footer and started on pages

---
+++ application/src/main/resources/static/js/react-client/src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./style.module.scss"
+
+const Footer = () =>{
+    return (
+        <div className={style.footer}>
+            <p>This is the footer</p>
+        </div>
+    )
+}
+
+export default Footer;
--- application/src/main/resources/static/js/react-client/src/pages/home/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/home/index.js
@@ -2,7 +2,15 @@ import React from "react"
 
 const Home = () => {
     return(
-        ""
+        <div>
+            <div>
+                <h1>Think Differently</h1>
+                <p>This is a paragraph</p>
+                <a href={/pagesadhd}>ADHD</a>
+                <button>Autism</button>
+
+            </div>
+        </div>
     )
 }
 
