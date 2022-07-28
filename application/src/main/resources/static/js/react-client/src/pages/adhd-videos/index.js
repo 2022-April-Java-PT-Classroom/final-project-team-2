ef7fc86726516cba0ab32fbc9ba4536228fad7f0
pennyschuenke <pennyschuenke@gmail.com>
2022-07-28 00:00:45 -0400

    apivideosalmostdone

---
+++ application/src/main/resources/static/js/react-client/src/pages/adhd-videos/index.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import style from "./style.module.scss";
+import Axios  from "axios";
+// import gapi from "gapi"
+
+
+
+const AdhdVideos =()=>{
+    
+    const [video,setVideo] = useState(null);
+    const [loading,setLoading]= useState(true);
+
+    // fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=PLhIJgpRusByNX2XARV4jJ29N1J_T3aZ8l&key=AIzaSyB6rAyis1rjqO7ch1ZS-Z3kCU5SJKnRmVc")
+    //   .then(response => response.json())
+    //   .then(jsonData => video = jsonData.VideoId)
+    //   .catch(err => console.log(err));
+
+
+    useEffect(()=>{
+        const fetchData = async () =>{
+            const result = await Axios("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=PLhIJgpRusByNX2XARV4jJ29N1J_T3aZ8l&key=AIzaSyB6rAyis1rjqO7ch1ZS-Z3kCU5SJKnRmVc");
+            setVideo(result.data);
+
+            console.log(result.data.items);
+        }
+        if (video){
+            setLoading(false);
+        }
+        const timer = setTimeout(()=>{
+            !video && fetchData();
+        },500);
+
+        return () => clearTimeout(timer);
+    }, [video]);
+  
+     return(
+        loading ? <h3>Loading . . .</h3> :
+        <div className={style.AdhdVideo}>
+            
+                <section>
+                  <div className={style.AdhdVideosContainer}>
+                    {/* <h1 id="video" src={video.items.url}></h1> */}
+                     <iframe id="video"  src={video.items.videoId}></iframe>
+                     <h1 id="title">title: {video.title}</h1>
+                    </div>
+                </section>
+            
+        </div>
+    );
+}
+export default AdhdVideos;
