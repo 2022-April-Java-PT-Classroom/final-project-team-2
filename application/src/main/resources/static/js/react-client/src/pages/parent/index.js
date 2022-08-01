49c27bf265a684eba2df2963971975c340d243a4
Mark Sypniewski <103773011+mark-sypniewski@users.noreply.github.com>
2022-07-31 23:01:57 -0400

    basic parent styling and links

--- application/src/main/resources/static/js/react-client/src/pages/parent/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/parent/index.js
@@ -1,20 +1,32 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import style from "./style.module.scss";
+import logo from "../../assest/logo.png";
+import cloud1 from "../../assest/cloud1.png";
+import cloud2 from "../../assest/cloud2.png";
 
 const Parent =()=>{
-    return(  <div>
-        <div>
-            <section>
-                <div>
-                    <h1>Parents Home</h1>
+    return(
+     <div className={style.parent}>
+       <section className={style.parentSection}>
+         <h1>Parent Resources</h1>
+          <div className={style.homeLogo}>
+                <img src={logo} alt={"logo"}></img>
                 </div>
-                <ul>
-                    <li>Child's Emotion Tracker</li>
-                    <li>ADHD Resources</li>
-                    <li>Autism Resources</li>
-                    <li></li>
-                </ul>
-            </section>
-        </div>
+          <div className={style.homeADDbtn}>
+                <h2>ADHD Resources</h2>
+                <Link to={"/adhd-resources"}><img src={cloud1} alt={"cloud1"}></img></Link>
+                
+                </div>
+                <div className={style.homeAutBtn}>
+                <Link to={'/autism-resources'}><img src={cloud2} alt={"cloud2"}></img></Link>
+                <h2>Autism Resources</h2>
+                </div>
+       </section>
+        
+        <section classname={style.moodSection}>
+            <h1>Mood Tracker</h1>
+        </section>
     </div>);
 }
 
