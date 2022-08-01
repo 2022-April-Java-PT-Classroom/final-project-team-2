2c9c7f07e88a40ba3f593aeeba49c9691e76ee98
danefirestone <danefirestone@gmail.com>
2022-08-01 03:38:52 -0400

    Created some const functions

--- application/src/main/resources/static/js/react-client/src/pages/calendar/src/App/App.jsx
+++ application/src/main/resources/static/js/react-client/src/pages/calendar/src/App/App.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
+
+    const [nav, setNav] = useState(0);
+    const [days, setDays] = useState([]);
+    const [dateDisplay, setDateDisplay] = useState('');
+    const [clicked, setClicked] = useState();
+    const [events, setEvents] = useState(
+        localStorage.getItem('events') ? 
+        JSON.parse(localStorage.getItem('events')) : []
+    );
+
+    const eventForDate = date => events.find(e => e.date === date);
+
+
     return(<>Hello from the App Component</>);
 };
