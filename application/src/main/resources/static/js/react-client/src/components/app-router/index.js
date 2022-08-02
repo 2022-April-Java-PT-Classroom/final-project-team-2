9f99f6d89f085156eaa049c10b860f24b57bc6e5
Mark Sypniewski <103773011+mark-sypniewski@users.noreply.github.com>
2022-08-02 00:57:49 -0400

    routed and linked resources

--- application/src/main/resources/static/js/react-client/src/components/app-router/index.js
+++ application/src/main/resources/static/js/react-client/src/components/app-router/index.js
@@ -3,15 +3,13 @@ import Timer from '../../pages/timer';
 import About from '../../pages/about';
 import Adhd from '../../pages/adhd';
 import AdhdVideos from '../../pages/adhd-videos';
+import AdhdResources from '../../pages/adhd-resources';
 import Autism from '../../pages/autism';
+import AutismResources from '../../pages/autism-resources';
 import Home from '../../pages/home';
 import Parent from '../../pages/parent';
 import Quotes from '../../pages/quotes';
 import React from "react";
-
-// import AdhdVideos from '../../pages/adhd-videos';
-
-
 
 
 const AppRouter = () => {
@@ -25,11 +23,9 @@ const AppRouter = () => {
             <Route exact path={'/about'} component={About} />
             <Route exact path={'/pages/quotes'} component={Quotes} /> 
             <Route path={'/adhd-videos'} component={AdhdVideos}/>
-            <Route path={'/adhd-videos'} component={Adhd}/>
+            <Route path={'/adhd-resources'} component={AdhdResources}/>
+            <Route path={'/autism-resources'} component={AutismResources}/>
             <Route path={'/timer'} component={Timer}/>
-            {/* <Route path={'/planets'} component={Planet} />
-            <Route path={'/add-planet'} component={AddPlanet} />
-            <Route path={"/edit-planet/:id"} component={AddPlanet} /> */}
         </Switch>
         
     );
--- application/src/main/resources/static/js/react-client/src/pages/adhd-resources/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/adhd-resources/index.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Axios from "axios";
 import style from './style.module.scss';
+
+
 const AdhdResources =()=>{
-return(  <div>
-    <div>
-        <section>
-            <div>
-                <h1>ADHD Resources</h1>
-            </div>
-            <ul>
-                <li>Books</li>
-                <li>Websites</li>
-                <li>Videos</li>
-            </ul>
-        </section>
-    </div>
-</div>);
+
+  
+
+        return(  <div>
+         <div>
+             <section>
+                 <div>
+                     <h1>ADHD Resources</h1>
+                    </div>
+                 <ul>
+                        <li>Books</li>
+                        <li>Websites</li>
+                        <li>Videos</li>
+                  </ul>
+            </section>
+        </div>
+    </div>);
 }
 export default AdhdResources;
