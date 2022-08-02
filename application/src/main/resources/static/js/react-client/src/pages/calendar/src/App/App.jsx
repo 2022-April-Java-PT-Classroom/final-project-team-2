f7d6ee6d82381a3fecbfc06166225d0c5578ffd9
danefirestone <danefirestone@gmail.com>
2022-08-02 00:03:07 -0400

    Created days array and for loop

--- application/src/main/resources/static/js/react-client/src/pages/calendar/src/App/App.jsx
+++ application/src/main/resources/static/js/react-client/src/pages/calendar/src/App/App.jsx
@@ -9,45 +9,95 @@ export const App = () => {
     const [dateDisplay, setDateDisplay] = useState('');
     const [clicked, setClicked] = useState();
     const [events, setEvents] = useState(
-        localStorage.getItem('events') ? 
-        JSON.parse(localStorage.getItem('events')) : []
+        localStorage.getItem('events') ?
+            JSON.parse(localStorage.getItem('events')) : []
     );
 
     const eventForDate = date => events.find(e => e.date === date);
 
     useEffect(() => {
-        localStorage.setItem('events', JSON.stringify(events);
+        localStorage.setItem('events'), JSON.stringify(events);
     }, [events]);
 
+    useEffect(() => {
+        const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const dt = new Date();
 
-    return(
-    <div id="container">
-        <CalendarHeader />
+        if (nav !== 0) {
+            dt.setMonth(new Date().getMonth() + nav);
+        }
+
+        const day = dt.getDate();
+        const month = dt.getMonth();
+        const year = dt.getFullYear();
+
+        const firstDayOfMonth = new Date(year, month, 1);
+        const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+        const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+        });
+        setDateDisplay(`${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`);
+        const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+        const daysArr = [];
+
+        for (let i = i; i <= paddingDays + daysInMonth; i++) {
+            const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+            if (i > paddingDays) {
+                daysArr.push({
+                    value: i - paddingDays,
+                    event: eventForDate(dayString),
+                    isCurrentDay: i - paddingDays === day && nav === 0,
+                    date: dayString,
+                });
+            } else {
+                daysArr.push({
+                    value: 'padding',
+                    event: null,
+                    isCurrentDay: false,
+                    date: '',
+                });
+            }
+        }
+
+        setDays(daysArr);
+
+    }, [events, nav]);
 
 
-    <div id="weekdays">
-      <div>Sunday</div>
-      <div>Monday</div>
-      <div>Tuesday</div>
-      <div>Wednesday</div>
-      <div>Thursday</div>
-      <div>Friday</div>
-      <div>Saturday</div>
-    </div>
+    return (
+        <div id="container">
+            <CalendarHeader />
 
-    <div id="calendar">
-        {days.map((d, index) => (
-            <Day 
-                key={index}
-                day={d}
-                onClick={() => {
-                    if (day.value !== 'padding') {
-                        setClicked(day.date);
-                    }
-                }}
-                
-            />
-        ))}
-    </div>
-  </div>);
+
+            <div id="weekdays">
+                <div>Sunday</div>
+                <div>Monday</div>
+                <div>Tuesday</div>
+                <div>Wednesday</div>
+                <div>Thursday</div>
+                <div>Friday</div>
+                <div>Saturday</div>
+            </div>
+
+            <div id="calendar">
+                {days.map((d, index) => (
+                    <Day
+                        key={index}
+                        day={d}
+                        onClick={() => {
+                            if (day.value !== 'padding') {
+                                setClicked(day.date);
+                            }
+                        }}
+
+                    />
+                ))}
+            </div>
+        </div>);
 };
