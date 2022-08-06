0728d5bf9d63b24e0744b54f5a3e420bfbfed49c
Mark Sypniewski <103773011+mark-sypniewski@users.noreply.github.com>
2022-08-06 16:45:51 -0400

    tryagain

--- application/src/main/java/org/wecancodeit/serverside/Repositories/AdhdtedxvideoRepository.java
+++ application/src/main/java/org/wecancodeit/serverside/Repositories/AdhdtedxvideoRepository.java
@@ -1,2 +1,10 @@
-package org.wecancodeit.serverside.Repositories;public interface AdhdtedxvideoRepository {
+package org.wecancodeit.serverside.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wecancodeit.serverside.Models.Adhdtedxvideo;
+
+import java.util.Optional;
+
+public interface AdhdtedxvideoRepository extends CrudRepository<Adhdtedxvideo, Long> {
+    Optional<Adhdtedxvideo> findByTitle(String AdhdtedxvideoTitle);
 }
--- application/src/main/java/org/wecancodeit/serverside/Repositories/AdhdwebsiteRepository.java
+++ application/src/main/java/org/wecancodeit/serverside/Repositories/AdhdwebsiteRepository.java
@@ -1,2 +1,10 @@
-package org.wecancodeit.serverside.Repositories;public interface AdhdwebsiteRepository {
+package org.wecancodeit.serverside.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wecancodeit.serverside.Models.Adhdwebsite;
+
+import java.util.Optional;
+
+public interface AdhdwebsiteRepository extends CrudRepository<Adhdwebsite, Long> {
+    Optional<Adhdwebsite> findByTitle(String AdhdwebsiteTitle);
 }
