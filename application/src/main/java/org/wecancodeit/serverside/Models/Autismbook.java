99d3cf3e648dc66aba38d8a834539e37243f3628
Mark Sypniewski <103773011+mark-sypniewski@users.noreply.github.com>
2022-08-06 22:52:16 -0400

    back

--- application/src/main/java/org/wecancodeit/serverside/Models/Autismbook.java
+++ application/src/main/java/org/wecancodeit/serverside/Models/Autismbook.java
@@ -1,2 +1,28 @@
-package org.wecancodeit.serverside.Models;public class Autismbook {
+package org.wecancodeit.serverside.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Autismbook {
+
+    @Id
+    @GeneratedValue
+    public Long id;
+    public String title;
+    public String author;
+
+    public Autismbook() {
+    }
+
+    public Autismbook(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {return id;}
+    public String getTitle() {return title;}
+    public String getAuthor() {return author;}
 }
