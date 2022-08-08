7bd64cb88f95feeb3329f267002cb496e8cbbb73
samleivadev <samdleiva@gmail.com>
2022-08-08 18:55:04 -0400

    styling almost finish, need add picture

--- application/src/main/resources/static/js/react-client/src/components/bios/biosCard.js
+++ application/src/main/resources/static/js/react-client/src/components/bios/biosCard.js
@@ -2,19 +2,25 @@ import React from "react";
 import style from "../bios/style.module.scss"
 
 function BioCard(props){
-    const {bioiImages, bioTitle, bioDescription, bioSkills, gitHub,linkedIn} = props
+    const {bioName, imageDev, bioTitle, bioDescription, bioSkills, gitHub} = props
+    // let bioPic = `url(${props.image})`
   return(
     <div className={style.bioCard}>
         <div className={style.upperContainer}>
-            <div className={style.imageDev}>{bioiImages}</div>
+            <div className={style.imgContainer}>
+            <img src={imageDev} alt="" height='100px' width="100px" />
+            {/* <div className={style.imageDev} style={{"background-Image": bioPic}}></div> */}
+        </div>
         </div>
         <div className={style.lowerContainer}>
+            <div className={style.devName}>{bioName}</div>
             <div className={style.devTitle}>{bioTitle}</div>
             <div className={style.devDesc}>{bioDescription}</div>
             <div className={style.devSkills}>{bioSkills}</div>
-            <div className={style.devGit}>{gitHub}</div>
-            <div className={style.devLinked}>{linkedIn}</div>
-            
+            <div className={style.buttons}>
+            <button >{gitHub}</button>
+            {/* <button >{linkedIn}</button> */}
+            </div>
                 
         </div>
     </div>
--- application/src/main/resources/static/js/react-client/src/pages/about/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/about/index.js
@@ -1,5 +1,7 @@
 import BioCard from "../../components/bios/biosCard";
 import React from "react";
+import SamLeiva from "../../assets/bioPics/SamLeiva.jpg"
+import style from "../about/style.module.scss";
 
 const About =()=>{
     return(  <div>
@@ -10,46 +12,69 @@ const About =()=>{
                 </div>
                 
             </section>
+            <div className={style.developers}>
             <BioCard
-                bioImages="../images/bios/name.png"
+                
+                bioName="Developer Name"
+                imageDev ='../../assets/bioPics/SamLeiva.jpg'
                 bioTitle="Full-Stack Software Developer"
                 bioDescription="et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum"
                 bioSkills="Java, CSS, HTML, JavaScript, React, Node"
-                gitHub="github.com/username"
-                linkedIn="linkedin.com/username"
+                gitHub="Website"
+              
             />
             <BioCard
-                bioImages="../images/bios/name.png"
+                
+                bioName="Developer Name"
+                imageDev ='../../assets/bioPics/SamLeiva.jpg'
                 bioTitle="Full-Stack Software Developer"
                 bioDescription="et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum"
                 bioSkills="Java, CSS, HTML, JavaScript, React, Node"
-                gitHub="github.com/username"
-                linkedIn="linkedin.com/username"
+                gitHub="Website"
+              
             />
             <BioCard
-                bioImages="../images/bios/name.png"
+                
+                bioName="Developer Name"
+                imageDev ='../../assets/bioPics/SamLeiva.jpg'
                 bioTitle="Full-Stack Software Developer"
                 bioDescription="et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum"
                 bioSkills="Java, CSS, HTML, JavaScript, React, Node"
-                gitHub="github.com/username"
-                linkedIn="linkedin.com/username"
+                gitHub="Website"
+              
             />
             <BioCard
-                bioImages="../images/bios/name.png"
+                
+                bioName="Developer Name"
+                imageDev ='../../assets/bioPics/SamLeiva.jpg'
                 bioTitle="Full-Stack Software Developer"
                 bioDescription="et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum"
                 bioSkills="Java, CSS, HTML, JavaScript, React, Node"
-                gitHub="github.com/username"
-                linkedIn="linkedin.com/username"
+                gitHub="Website"
+              
             />
             <BioCard
-                bioImages="../images/bios/name.png"
+                
+                bioName="Developer Name"
+                imageDev ='../../assets/bioPics/SamLeiva.jpg'
                 bioTitle="Full-Stack Software Developer"
                 bioDescription="et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum"
                 bioSkills="Java, CSS, HTML, JavaScript, React, Node"
-                gitHub="github.com/username"
-                linkedIn="linkedin.com/username"
+                gitHub="Website"
+              
             />
+            <BioCard
+                
+                bioName="Developer Name"
+                imageDev ='../../assets/bioPics/SamLeiva.jpg'
+                bioTitle="Full-Stack Software Developer"
+                bioDescription="et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum"
+                bioSkills="Java, CSS, HTML, JavaScript, React, Node"
+                gitHub="Website"
+              
+            />
+            
+            </div>
             
         </div>
     </div>);
