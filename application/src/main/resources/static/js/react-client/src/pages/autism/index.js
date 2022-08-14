6b5ba4daddd6597a6d8f26b9d702d32ae4694e84
danefirestone <danefirestone@gmail.com>
2022-08-13 21:37:16 -0400

    Added new images for wip and spinner

--- application/src/main/resources/static/js/react-client/src/pages/autism/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/autism/index.js
@@ -5,11 +5,12 @@ import style from "./style.module.scss";
 import logo from "../../assets/logo.png";
 import {Link} from "react-router-dom";
 import spinner1 from "../../assets/spinner1.png";
+import wip from "../../assets/wip.png";
 
 
 const Autism =()=>{
     return(
-        <div className={style.parent}>
+        <div className={style.autism}>
       <div className={style.parentHero}>
           <div className={style.pTitle}>Autism Tool Box
           
@@ -24,6 +25,10 @@ const Autism =()=>{
           <div className={style.spinnerbtn}>
                 <Link to={"/fidget-spinner"}><img src={spinner1} alt={"spinner1"}></img></Link>
                 <h2>Fidget Spinner</h2>
+                </div>
+                <div className={style.homeAutBtn}>
+                <Link to={'/autism-resources'}><img src={wip} alt={"wip"}></img></Link>
+                <h2>Autism Resources</h2>
                 </div>
                
        </section>
