0619bb8765090fc175cd66ba9ff25c4b0fb4a4bb
danefirestone <danefirestone@gmail.com>
2022-08-13 20:22:10 -0400

    strted on autism page

--- application/src/main/resources/static/js/react-client/src/pages/autism/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/autism/index.js
@@ -2,31 +2,32 @@ import EmotionsApp from '../../components/emotions/emotionsApp';
 import NotesApp from '../../components/notes/notesApp';
 import React from "react";
 import style from "./style.module.scss";
+import logo from "../../assets/logo.png";
+import {Link} from "react-router-dom";
+import spinner1 from "../../assets/spinner1.png";
+
 
 const Autism =()=>{
-    return(  <div>
-        <div>
-            <section>
-                <div>
-                    <h1>Autism tool box</h1>
+    return(
+        <div className={style.parent}>
+      <div className={style.parentHero}>
+          <div className={style.pTitle}>Autism Tool Box
+          
+          </div>
+          <div className={style.pLogo}>
+              <img src={logo} alt={"logo"}></img>
+          </div>
+        
+      </div>
+       <section className={style.parentSection}>
+        
+          <div className={style.spinnerbtn}>
+                <Link to={"/fidget-spinner"}><img src={spinner1} alt={"spinner1"}></img></Link>
+                <h2>Fidget Spinner</h2>
                 </div>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </section>
-            <div className={style.components}>
-                    <div className={style.emoComp}>
-                    <EmotionsApp/>
-                    </div>
-                    <div className={style.notesComp}>
-                    <NotesApp/>
-                    </div>
-                </div>
-        </div>
-    </div>);
-
+               
+       </section>
+       </div>
+    );
 }
 export default Autism;
