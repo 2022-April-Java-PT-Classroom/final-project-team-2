b8a4ddc79c99b2ffd8f066f57a0da54d7388b51f
JNegron300 <metal_x40@live.com>
2022-08-15 19:26:49 -0400

    Bio image upload

--- application/src/main/resources/static/js/react-client/src/pages/about/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/about/index.js
@@ -51,11 +51,12 @@ const About =()=>{
             <BioCard
                 
                 bioName="Jimmy Negron"
-                imageDev ='https://media-exp1.licdn.com/dms/image/C5603AQGcI2M6zjz2fQ/profile-displayphoto-shrink_400_400/0/1651257482340?e=1665619200&v=beta&t=z4TqBtP6KTMpgLBmyMTGcixIp6E8vIvriNftoGXqV-s'
+                imageDev ='https://media-exp1.licdn.com/dms/image/D5635AQFoXUCM8D4vtw/profile-framedphoto-shrink_200_200/0/1660170763315?e=1661212800&amp;v=beta&amp;t=pZO4r04S2SM5GFrjl5meAxZ8DclQ1NGi-AmtftJbiMI'
                 bioTitle="Full-Stack Software Developer"
                 bioDescription="et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum"
                 // bioSkills="Java, CSS, HTML, JavaScript, React, Node"
                 gitHub="Website"
+            
               
             />
             <BioCard
